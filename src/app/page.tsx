6c7d305c4151c7a5eaee3397ba2919474ba793de
Bhavesh  Chand Upadhyay <94699084+Visbhavesh@users.noreply.github.com>
2024-12-28 15:18:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
       <main className="min-h-screen bg-black/[0.99] antialiased bg-grid-white/[0.04] ">
       <HeroSection/>
-      <TeacherSection/>
+      {/* <TeacherSection/> */}
       <FeaturedCourses/>
       <WhyChooseUs/>
       <SchoolTestimonials/>
