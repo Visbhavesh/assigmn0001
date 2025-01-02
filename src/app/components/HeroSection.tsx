069d7f9fed7bd0cad07796dc6e6eb171cc87dd5d
Bhavesh  Chand Upadhyay <94699084+Visbhavesh@users.noreply.github.com>
2025-01-02 14:23:29 +0530

    Update HeroSection.tsx

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,11 +1,17 @@
+
+
+"use client";
+
 import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border"
 
 
-function HeroSection() {
+function 
+
+HeroSection() {
     return (
-        <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative 
+        <div className="h-[100vh] w-full rounded-md flex flex-col items-center justify-center relative 
         overflow-hidden mx-auto py-10 md:py-0"> 
         <div className="p-4 relative z-10 w-full text-center">
             </div>
