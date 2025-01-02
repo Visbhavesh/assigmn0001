39f14387f1d99e5463c8992adc3c15a396ebf3f3
Bhavesh  Chand Upadhyay <94699084+Visbhavesh@users.noreply.github.com>
2025-01-02 13:59:44 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import HeroSection from "./components/HeroSection";
 import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/WhyChooseUs";
@@ -7,17 +8,29 @@ import Instructor from "./components/Instructors";
 import Footer from "./components/Footer";
 import TeacherSection from "./components/FeaturedCourses";
 
+const pageVariants = {
+  initial: { opacity: 0, x: "-100%" },
+  animate: { opacity: 1, x: 0, transition: { duration: 0.8 } },
+  exit: { opacity: 0, x: "100%", transition: { duration: 0.8 } },
+};
+
 export default function Home() {
   return (
-      <main className="min-h-screen bg-black/[0.99] antialiased bg-grid-white/[0.04] ">
-      <HeroSection/>
+    <motion.main
+      className="min-h-screen bg-black/[0.99] antialiased bg-grid-white/[0.04]"
+      variants={pageVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      <HeroSection />
       {/* <TeacherSection/> */}
-{/*       <FeaturedCourses/> */}
-      {/*   <WhyChooseUs/>*/}
-      <SchoolTestimonials/>
-      <UpcomingWebinar/>
-      <Instructor/>
-      <Footer/>
-      </main>
+      {/* <FeaturedCourses/> */}
+      <WhyChooseUs />
+      <SchoolTestimonials />
+      <UpcomingWebinar />
+      <Instructor />
+      <Footer />
+    </motion.main>
   );
 }
